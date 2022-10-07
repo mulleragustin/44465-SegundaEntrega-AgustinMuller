87008fb7b4d12aaf93827011c27e8a480a6d2313
mulleragustin <agustinmuller98@gmail.com>
2022-10-06 22:20:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+import { productos } from "./stock.js";
+
+//-------------
+let carrito = [];
+const mostrarproductos = (productos) => {
+    const divproductos = document.getElementById("listaproductos");
+
+    productos.forEach(producto => {
+        const div = document.createElement(`div`)
+        div.classList.add("col-sm-3")
+        div.innerHTML += `
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title text-center"><img id="img_obs" src="${producto.img}"></h5>
+                        <p class="card-text"><br>${producto.nombre}<br>$  ${producto.precio}</p>
+                        <p class="card-text text-center">
+                        <a data-toggle="modal" data-target="#exampleModal3" id=boton${producto.id}  href="#" class="btn btn-primary">Añadir al Carrito</a>
+                        </p>
+                    </div>
+                </div>
+        `
+        divproductos.appendChild(div)
+        
+        const boton = document.getElementById(`boton${producto.id}`)
+        boton.addEventListener('click', ()=>{
+        agregarAlCarrito(producto.id)
+        alert(`Se agrego ${producto.nombre}`) })
+    })
+}
+const agregarAlCarrito = (prodId) => {
+    const item = productos.find((prod) => prod.id === prodId)
+    carrito.push(item)
+    const divCarrito = document.getElementById("listaCarrito")
+    const renderCarrito = () =>{
+        const div = document.createElement(`div`)
+        div.classList.add("col-sm-3")
+        div.innerHTML += `
+                <li>
+                    
+                       <img id="img_obs" src="${item.img}">${item.nombre}$  ${item.precio}
+                        
+                        
+                    
+                </li>
+        `
+        divCarrito.appendChild(div)
+        
+    }
+    
+    renderCarrito()
+    console.log(carrito)
+    const limpiarCarrito = document.getElementById("limpiar")
+    limpiarCarrito.addEventListener('click', () => divCarrito.innerHTML ='')
+}
+
+mostrarproductos(productos);
+
+
---
+++ stock.js
@@ -0,0 +1,34 @@
+const productos = [
+    {
+        id:1,
+        nombre:"Alfajor",
+        precio:"75",
+        img:"http://d3ugyf2ht6aenh.cloudfront.net/stores/233/164/products/9a38be71-e4ad-452c-8658-1f405bf09ead1-5f6a1228cc3b5d3c3216329561849714-640-0.jpeg",
+    },
+    {
+        id:2,
+        nombre:"Coca-Cola",
+        precio:"300",
+        img:"https://m.media-amazon.com/images/I/5156FefjlqL._SX425_.jpg",
+    },
+    {
+        id:3,
+        nombre:"Fernet",
+        precio:"1000",
+        img:"https://www.fernetbranca.com/wp-content/uploads/2017/06/bottle.png",
+    },
+    {
+        id:4,
+        nombre:"Yerba",
+        precio:"500",
+        img:"https://cdn.newgarden.com.ar/media/catalog/product/cache/dda7253a1a2f6711745de410175d10f8/c/o/copia_de_verdeflor_hierbas_serranas_500_3-4.jpg",
+    },
+    {
+        id:5,
+        nombre:"Chocolate",
+        precio:"200",
+        img:"https://arcorencasa.com/wp-content/uploads/2021/06/20210603-13311.png",
+    },
+]
+
+export {productos};
